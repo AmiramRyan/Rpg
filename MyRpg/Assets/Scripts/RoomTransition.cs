ce5f0f981487bf3de9cb900299148c7082979c24
AmiramRyan <Ryan4works@gmail.com>
2021-09-04 15:23:18 +0300

    New map design 30x20
    map is now 30X20 each room
    first room nearly done
    Ues as base for others

--- MyRpg/Assets/Scripts/RoomTransition.cs
+++ MyRpg/Assets/Scripts/RoomTransition.cs
@@ -27,7 +27,7 @@ public class RoomTransition : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !other.isTrigger)
         {
             cam.minPos += camShift;
             cam.maxPos += camShift;
